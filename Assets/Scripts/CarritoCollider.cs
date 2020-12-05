312a6e5078be4f17e62ed7f44a022cbf25e0e373
Miguel Ferra <miguelferra4@gmail.com>
2020-12-04 20:43:46 -0700

    Reloj y cambios de colliders

--- Assets/Scripts/CarritoCollider.cs
+++ Assets/Scripts/CarritoCollider.cs
@@ -6,18 +6,21 @@ using SpeechLib;
 
 public class CarritoCollider : MonoBehaviour
 {
-    public GameObject puntuacion;
-    private int puntos = 0;
-    public AudioSource sonidoCarrito;
-	public AudioClip sonidoPapel;
-	public GameObject jugador;
-	public GameObject papel;
-	public GameObject scriptMov;
-
+	
 	private SpVoice voice;
-	public GameObject panel;
+	public GameObject jugador;
+
+	//Papel
+	public AudioClip sonidoPapel;
+	public AudioSource sonidoCarrito;
+	private int puntos = 0;
+	public GameObject puntuacion;
 	public GameObject panelFinal;
-	public GameObject panelGameover;
+	public GameObject scriptMov;
+	public GameObject fondoCarrito;
+	public GameObject papel;
+
+
 	// Start is called before the first frame update
 	void Start()
     {
@@ -25,57 +28,42 @@ public class CarritoCollider : MonoBehaviour
 	}
 
     // Update is called once per frame
-    void Update()
+    void Update()	
     {
-        
-    }
+		
+	}
+
+	
 
 	void OnTriggerEnter(Collider objeto)
 	{
+
+		if (objeto.gameObject.tag == "Spawn")
+		{
+			Debug.Log("Toque Spawn");
+			Instantiate(papel, new Vector3(fondoCarrito.transform.position.x, objeto.gameObject.transform.position.y - 1.5f, fondoCarrito.transform.position.z), Quaternion.identity);
+
+			objeto.gameObject.active = false;
+		}
+
 		if (objeto.gameObject.tag == "Papel")
 		{
 			Debug.Log("Toque Papel");
 			objeto.gameObject.transform.parent = jugador.transform;
-			sonidoCarrito.PlayOneShot(sonidoPapel);
 			objeto.enabled = false;
+			sonidoCarrito.PlayOneShot(sonidoPapel);
 			objeto.gameObject.GetComponent<Rigidbody>().isKinematic = true;
 			puntos++;
 			puntuacion.GetComponent<Text>().text = puntos.ToString();
 		}
-		if (objeto.gameObject.tag == "Spawn")
-		{
-			Debug.Log("Toque Spawn");
-			Instantiate(papel, new Vector3(objeto.gameObject.transform.position.x, objeto.gameObject.transform.position.y + 2, objeto.gameObject.transform.position.z), Quaternion.identity);
-			objeto.gameObject.active = false;
-		}
-		if (objeto.gameObject.tag == "Inicio")
-		{
-			Debug.Log("Toque Inicio");
-			voice.Speak("!!!Recolecta 10 rollos de papel antes de que te los ganen!!!" +
-				"Acercate a los destellos amarillos para recolectarlos", SpeechVoiceSpeakFlags.SVSFlagsAsync);
-			panel.gameObject.SetActive(true);
-		}
+
 		if (puntos == 10)
 		{
 			voice.Speak("Usted ha ganado apa", SpeechVoiceSpeakFlags.SVSFlagsAsync);
 			panelFinal.gameObject.SetActive(true);
 			scriptMov.active = false;
-		}
-        if (objeto.gameObject.tag == "Enemigo")
-        {
-			voice.Speak("Usted se ha infectado apa Quiere volver a jugar?", SpeechVoiceSpeakFlags.SVSFlagsAsync);
-			panelGameover.gameObject.SetActive(true);
-			jugador.active = false;
+
 		}
 	}
 
-	void OnTriggerExit(Collider col)
-	{
-		if (col.gameObject.tag == "Inicio")
-		{
-			panel.gameObject.SetActive(false);
-			col.gameObject.active = false;
-		}
-
-	}
 }
---
+++ Assets/Scripts/JugadorCollider.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SpeechLib;
+using UnityEngine.UI;
+
+public class JugadorCollider : MonoBehaviour
+{
+
+    private SpVoice voice;
+    public GameObject panel;
+    public GameObject panelGameover;
+    public GameObject jugador;
+    public GameObject panelTimer;
+
+    //Reloj
+    float theTime = 0;
+    float speed = 1f;
+    bool isTiming = false;
+    public Text textoTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        voice = new SpVoice();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTiming)
+        {
+            theTime += Time.deltaTime * speed;
+            string segundos = (theTime % 60).ToString("00");
+            string minutos = Mathf.Floor((theTime % 3600) / 60).ToString("00");
+            textoTimer.text = minutos + ":" + segundos;
+        }
+
+    }
+    void OnTriggerEnter(Collider objeto)
+    {
+
+        if (objeto.gameObject.tag == "Inicio")
+        {
+            Debug.Log("Toque Inicio");
+            voice.Speak("!!!Recolecta 10 rollos de papel antes de que te los ganen!!!" +
+                "Acercate a los destellos amarillos para recolectarlos", SpeechVoiceSpeakFlags.SVSFlagsAsync);
+            panel.gameObject.SetActive(true);
+        }
+        if (objeto.gameObject.tag == "Enemigo")
+        {
+            voice.Speak("Usted se ha infectado apa Quiere volver a jugar?", SpeechVoiceSpeakFlags.SVSFlagsAsync);
+            panelGameover.gameObject.SetActive(true);
+            jugador.active = false;
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Inicio")
+        {
+            panel.gameObject.SetActive(false);
+            col.gameObject.active = false;
+            panelTimer.active = true;
+            isTiming = true;
+        }
+    }
+}
